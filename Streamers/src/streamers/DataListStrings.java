d9eb9b61f8773b26ff8c8821d0081389eb135f70
Richard Hasting <rhastingjr@gmail.com>
2021-08-03 18:31:39 -0500

    Added first streams and lambdas to the project

---
+++ Streamers/src/streamers/DataListStrings.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package streamers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.*;
+
+/**
+ *
+ * @author richard
+ * 
+ * Mission: Place an array of String into a List
+ */
+public class DataListStrings {
+    
+  private ArrayList<String> dataList = new ArrayList<String>();
+
+    public ArrayList<String> getDataList() {
+        return dataList;
+    }
+
+    public static String[] getArray() {
+        return array;
+    }
+    
+static String [] array = {"aronical", "Aaronite", "Aaronitic", "Aaron's-beard","Aaronsburg", "Aaronson","AARP", "aarrgh","aarrghh", "Aaru","AAS",
+"A'asia","aasvogel", "aasvogels","AAU", "AAUP","AAUW", "AAVSO","AAX", "A-axes","A-axis", "AB","ab-", "ABA","Ababa", "Ababdeh","Ababua",
+"abac","abaca", "abacay","abacas", "abacate","abacaxi", "abaci","abacinate", "abacination","abacisci", "abaciscus","abacist", "aback",
+"abacli", "Abaco", "abacot", "abacterial", "abactinal", "abactinally", "abaction", "abactor", "abaculi", "abaculus", "abacus", "abacuses",
+"Abad", "abada", "Abadan", "Abaddon", "abadejo", "abadengo", "abadia", "Abadite", "abaff", "abaft", "Abagael", "Abagail", "Abagtha",
+"abay", "abayah", "Abailard", "abaisance", "abaised", "abaiser", "abaisse", "abaissed", "abaka", "Abakan", "abakas", "Abakumov", "abalation",
+"abalienate", "abalienated", "abalienating", "abalienation", "abalone", "abalones", "Abama", "abamp", "abampere", "abamperes", "abamps",
+"Abana", "aband", "abandon", "abandonable", "abandoned", "abandonedly", "abandonee", "abandoner", "abandoners", "abandoning", "abandonment",
+"abandonments", "abandons", "abandum", "abanet", "abanga", "Abanic", "abannition", "Abantes", "abapical", "abaptiston", "abaptistum",
+"Abarambo", "Abarbarea", "Abaris", "abarthrosis", "abarticular", "abarticulation", "Abas", "abase", "abased", "abasedly", "abasedness",
+"abasement", "abasements", "abaser", "abasers", "abases", "Abasgi", "abash", "abashed", "abashedly", "abashedness", "abashes",
+"abashing", "abashless", "abashlessly", "abashment", "abashments", "abasia", "abasias", "abasic", "abasing", "abasio", "abask",
+"abassi", "Abassieh", "Abassin"};
+    
+    // The new way! 
+    public DataListStrings()
+    {
+        Arrays.stream(array).forEach(dataList::add);
+    }
+    
+    // the old way
+    public DataListStrings(Boolean dolt)
+    {
+        if (dolt)
+            for(var i = 0; i < array.length; i++)
+                dataList.add(array[i]);
+    }    
+}
---
+++ Streamers/src/streamers/Streamer1.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package streamers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author richard
+ */
+public class Streamer1 {
+    
+    
+    // Mission: eliminate all words with a length of 5. sort them, count 
+    // and print the total number of letters in the list, and then have a 
+    // new list without the 5 letter words
+    public static void test(String[] args) {
+        DataListStrings dls = new DataListStrings();
+        List<String> dataList = dls.getDataList();
+        
+        System.out.println("dataList has "+ dataList.size() + " words in it");
+        // old way
+        ArrayList <String> newList = new ArrayList<>();
+        int foo = 0;
+        int count = 0;
+        for (String line : dataList)
+            if (line.length() == 5) 
+                foo ++;
+            else{
+                newList.add(line);
+                count += line.length();
+            }
+        
+        System.out.println("dataList has "+ foo + " 5 letter words in it");
+        System.out.println(count + "\n" + newList);
+  
+        newList.clear();
+        foo = (int) dataList.parallelStream().filter(l -> l.length() == 5)
+                .count();
+        // the new way
+        dataList.stream().filter(l -> l.length() !=5 )
+                .forEach(l-> {newList.add(l);});
+        // and in parallel too
+        
+        count = newList.parallelStream().
+                filter(l -> l.length() != 5)
+                .mapToInt(l-> l.length())
+                .sum();
+                
+         
+        System.out.println("dataList has "+ foo + " 5 letter words in it");
+        
+        System.out.println(count + "\n" + newList);
+        
+    }
+    
+    public static void main(String[] args) {
+        test(args);
+    }
+}
